6eb4171ef481c03ae5aa43ca53271be04fc977e3
Brijendra23 <brijendra.singh.bce20@iitbhu.ac.in>
2023-08-21 02:30:21 +0530

    changes

--- Assets/ObstacleSpwaner.cs
+++ Assets/ObstacleSpwaner.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ObstacleSpwaner : MonoBehaviour
 {
+    [SerializeField] private GameObject[] obstacle;
+    [SerializeField] private GameObject player;
+    [SerializeField]private float spawnX=10f;
+    private bool hasDed; 
+    
+    
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("ObstacleSpawn", 3, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void ObstacleSpawn()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnX,spawnX), 0.0f, 30.0f);
+        int ObstacleIndex = Random.Range(0, obstacle.Length);
+        Instantiate(obstacle[ObstacleIndex],spawnPos, Quaternion.Euler(0,0,0));
         
     }
 }
--- Assets/Scripts/Building_Spawner.cs
+++ Assets/Scripts/Building_Spawner.cs
@@ -6,11 +6,11 @@ public class Building_Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] building;
     [SerializeField] private GameObject player;
+    private bool playerHasDied;
     
     void Start()
     {
-        buildingSpawner();
-
+        InvokeRepeating("buildingSpawner", 0, 0.5f);
 
     }
 
@@ -21,20 +21,8 @@ public class Building_Spawner : MonoBehaviour
     } 
     void buildingSpawner()
     {
-        Vector3 zPos = new Vector3(-30.0f, 0.0f, 30.0f);
-        Vector3 xPos= new Vector3 (30.0f, 0.0f, 30.0f);
-        Vector3 posChange = new Vector3(0.0f, 0.0f, 40.0f);
-
-        while (zPos.z < 1500.0f)
-
-        {
-            int buildingIndex = Random.Range(0, building.Length);
-            int buildingIndex2 = Random.Range(0, building.Length);
-            Instantiate(building[buildingIndex], zPos, Quaternion.Euler(0, -90, 0));
-            Instantiate(building[buildingIndex], xPos, Quaternion.Euler(0, 90, 0));
-            zPos.z += 40.0f;
-            xPos.z += 40.0f;
-        }
+       
+        Instantiate(building);
 
 
 
@@ -46,34 +34,7 @@ public class Building_Spawner : MonoBehaviour
 
 
 
-
-      /*  Vector3 z = new Vector3(0.0f, 0.0f, 20f);
-        float x = 0.0f;
-        while (x<1500.0f)// using the player in the scene
-        {
            
-
-            int buildingIndex = Random.Range(0, building.Length);
-            int buildingIndex2= Random.Range(0, building.Length);
-            Vector3 posX = new Vector3(-30f , 0.0f,20f);
-            Vector3 posZ = new Vector3(+30f, 0.0f, 20f);
-            Instantiate(building[buildingIndex], posZ, Quaternion.Euler(0,-90,0));
-            Instantiate(building[buildingIndex2],posX, Quaternion.Euler(0, 90, 0));
-
-            posX = posX + z;
-            posZ = posZ + z;
-            x = x + 20;
-          
-            // use tag and player position to delete the extra passed buildings
-            GameObject[] build = GameObject.FindGameObjectsWithTag("Building");
-            for (int i = 0; i < build.Length; i++)
-            {
-                if (build[i].transform.position.z < (player.transform.position.z - 10))
-                {
-                    Destroy(build[i]);
-                }
-            }*/
-           
-        }
+     }
     
 }
