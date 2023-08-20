8bc55f8a88165e0c166b2b25a9e5bb9ad9579f68
Yash-Agarwal1708 <yash.agarwal.phy21@iitbhu.ac.in>
2023-08-21 01:59:21 +0530

    cutscene_almost_dn

---
+++ Assets/Scripts/grenade.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grenade : MonoBehaviour
+{
+    public float speed = 100f;
+    private Vector3 pos;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        pos.x=target.position.x;
+        pos.y = target.position.y + 50f;
+        pos.z = target.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, pos, step);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            print("hello");
+            Destroy(gameObject);
+            other.gameObject.GetComponent<Animator>().SetBool("Death_b", true);
+            other.gameObject.GetComponent<Animator>().SetInteger("DeathType_int", 2);
+            
+        }
+    }
+}
---
+++ Assets/finalCutsceneDialogue.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class finalCutsceneDialogue : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+    public string[] Sentences;
+    private int Index;
+    public float speed;
+    public Animator dialogueAnim;
+    public Animator enemyAnim;
+    private bool startDialogue = true;
+    private bool isWriting = false;
+    public GameObject grenade;
+    private float bulletSpeed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (startDialogue)
+        {
+            dialogueAnim.SetTrigger("entry");
+            startDialogue = false;
+        }
+        isWriting = true;
+        NextSentence();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isWriting)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isWriting = true;
+                NextSentence();
+            }
+        }
+    }
+    void NextSentence()
+    {
+        if (Index <= Sentences.Length - 1)
+        {
+            dialogueText.text = "";
+            StartCoroutine(WriteSentences());
+        }
+        else
+        {
+            dialogueText.text = "";
+            dialogueAnim.SetTrigger("exit");
+            //SceneManager.LoadScene("SampleScene");
+            enemyAnim.SetInteger("WeaponType_int", 10);
+            StartCoroutine(Grenade());
+        }
+    }
+    IEnumerator WriteSentences()
+    {
+        foreach (char character in Sentences[Index].ToCharArray())
+        {
+            dialogueText.text += character;
+            yield return new WaitForSeconds(speed);
+        }
+        Index++;
+        isWriting = false;
+    }
+    IEnumerator Grenade()
+    {
+        yield return new WaitForSeconds(1.5f);
+        GameObject clone = Instantiate(grenade);
+        clone.transform.position = new Vector3(-109, 7, 213);
+        clone.AddComponent<grenade>();
+    }
+}
